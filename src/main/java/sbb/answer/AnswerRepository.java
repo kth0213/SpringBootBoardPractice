f6ebcc6edba310679cbff8c2136cebf879b3dd5c
kth0213 <asrai0412@google.com>
2025-01-05 14:17:53 +0900

    3-04

--- src/main/java/sbb/answer/AnswerRepository.java
+++
@@ -1,8 +0,0 @@
-package sbb.answer;
-
-import com.mysite.sbb.answer.Answer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnswerRepository extends JpaRepository<Answer, Integer> {
-
-}
